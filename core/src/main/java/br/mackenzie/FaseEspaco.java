18b8055480204c646cf2d186fa7a8c526bd5f899
Gabriel Labarca Del Bianco <sdbianco10@gmail.com>
2025-11-11 21:07:05 -0300

    Update FaseEspaco.java

--- core/src/main/java/br/mackenzie/FaseEspaco.java
+++ core/src/main/java/br/mackenzie/FaseEspaco.java
@@ -1,16 +1,12 @@
 package br.mackenzie;
 
-public class FaseEspaco extends FaseBase {
-    private final Main game;
-
-    public FaseEspaco(Main game){
-        super("Background-ParalaxFaseEspaco.png","GameMusic.mp3");
-        this.game = game;
+public class FaseEspaco extends FaseBase{
+    public FaseEspaco(FaseBaseScreen game) {
+        super("Imagens_Fase3/Fundo_fase3.png", "Sons_Fase3/GameMusic.mp3", game);
     }
 
-    private void voltarAoMenu() {
-        if (game != null) {
-            game.setScreen(new Menu(game));
-        }
+    @Override
+    public void render(float delta) {
+        super.render(delta);
     }
 }
